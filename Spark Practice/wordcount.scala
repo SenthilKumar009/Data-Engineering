d9d638008e46d6d0bd6440a64d60f24b8a7aabcd
Senthil Kumar Kanagaraj <senthilkumark.nitt@gmail.com>
2022-01-22 19:19:01 +0530

    Create wordcount.scala

---
+++ Spark Practice/wordcount.scala
@@ -0,0 +1,7 @@
+val input = sc.textFile("G:/BigData-OLC/DataSet/word1.txt")
+val words = input.flatMap(line => line.split(" "))
+val mapWord = words.map(line => (line, 1))
+val countWord = mapWord.reduceByKey(_+_)
+countWord.collect()
+
+countWord.saveAsTextFile("G:/BigData-OLC/DataSet/output/wordcount.txt")
